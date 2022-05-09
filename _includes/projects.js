1f7f112ad504b26935d97cbe001ba271599b4bd6
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-05-10 01:55:09 +0700

    project page

--- _includes/projects.js
+++ _includes/projects.js
@@ -6,14 +6,25 @@ let whenDocumentReady = (f) => {
 whenDocumentReady(isReady = () => {
 
 
-let xhrDone = (res) => {
-     res = JSON.parse(res)
-     console.log(res)
-     //document.getElementById("backpageprojects").innerHTML =  res.meta.pagination.total
+    let xhrDone = (res) => {
+        //parse the response
+        res = JSON.parse(res)
+        //get the datatable
+        var table = $('#dataTable').DataTable();
+        //loop through the data
+        for (var i = 0; i < res.data.length; ++i) {
+            //get the created date
+            let createdAt =  new Date(res.data[i].attributes.createdAt);
+            //convert, apis should give a formatted data option!
+            createdAt = `${createdAt.getDate()}/${createdAt.getDate()}/${createdAt.getFullYear()}`
+            //add the records
+            var rowNode = table
+                .row.add([res.data[i].id,res.data[i].attributes.name, createdAt, 'Edit'])
+                .draw()
+                .node();
+        }
+    }
+    //call the create account endpoint
+    xhrcall(1, "backpage-projects/", "", "json", "", xhrDone, token)
 
-
-}
-//call the create account endpoint
-xhrcall(1, "backpage-projects/", "", "json", "", xhrDone,token)
-
-});
+})
