1e52ec6cb89ac5009796ad78d8364199c88cfd7f
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-05-21 03:08:37 +0700

    template variabled and project data clean up

--- _includes/project-data.js
+++ _includes/project-data.js
@@ -51,48 +51,6 @@ whenDocumentReady(isReady = () => {
             //to fix
             $(table.column(i).footer()).text(keys[i]);
         }
-        /*
-
-    
- 
-    $('#projectdatatable').DataTable({
-        data: results,
-        columns: [
-            { title: 'Name' },
-            { title: 'Position' },
-            { title: 'Office' }
-        ],
-    });
-
-
-        //var table = $('#dataTable').DataTable();
-        //loop through the data
-
-        //data: aDemoItems
-
-        /*
-        for (var i = 0; i < res.data.length; ++i) {
-
-            let publishbutton = `<a href="/project/data/?id=${res.data[i].id}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-    <i class="fas fa-globe fa-sm text-white-50"></i> Publish</a>` 
-            let viewbutton = `<a href="/project/data/?id=${res.data[i].id}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-    <i class="fas fa-eye fa-sm text-white-50"></i> View</a>` 
-            let deletebutton = `<a href="javascript:deleteProject(${res.data[i].id})" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-    <i class="fas fa-trash fa-sm text-white-50"></i> Delete</a>`
-
-            //get the created date
-            let createdAt = new Date(res.data[i].attributes.createdAt);
-            //convert, apis should give a formatted data option!
-            createdAt = `${createdAt.getDate()}/${createdAt.getDate()}/${createdAt.getFullYear()}`
-
-            let data = JSON.stringify(res.data[i].attributes.data)
-            //add the records
-            var rowNode = table
-                .row.add([res.data[i].id, createdAt, data , ` ${viewbutton} ${deletebutton} ${publishbutton} `])
-                .draw()
-                .node();
-        }
-        */
     }
     //build the json
     let bodyobj = {
--- _includes/project-template.js
+++ _includes/project-template.js
@@ -18,9 +18,7 @@ let html5layout = `<DOCTYPE! html>
     < section >
     <header>Hello, friend</header>
     <article>
-      \{\{ element1 \}\}
-      \{\{ element2 \}\}
-      \{\{ element3 \}\}
+     [[ELEMENTS]]
     </article>
     <footer>@ whoever</footer>
   </section>
@@ -47,6 +45,71 @@ whenDocumentReady(isReady = () => {
         let xhrDone = (res) => {
             //parse the response
             res = JSON.parse(res)
+            let results = []
+            let keys;
+
+            for (var i = 0; i < res.data.length; ++i) {
+                keys = Object.keys(res.data[i].attributes.data)
+            }
+
+            //get the template
+            let xhrDone2 = (res) => {
+                //parse the response
+                res = JSON.parse(res)
+                let theCode = res.data.attributes.template;
+                if ((theCode == null) || (theCode == "")) {
+                    let elements="";
+                    for (var i = 0; i < keys.length; ++i) {
+                        //console.log(keys[i])
+                        elements = elements+`\{\{${keys[i]}\}\}<br>`
+                    }
+                    theCode = html5layout
+                    theCode = theCode.replace("[[ELEMENTS]]",elements);
+                    //it's default so change to add
+                    document.getElementById("btn-template").innerHTML = "Create"
+                } else
+                    document.getElementById("btn-template").innerHTML = "Update"
+
+
+                let textArea = document.getElementById('inp-projectemplate');
+                myCodeMirror = CodeMirror.fromTextArea(textArea);
+                myCodeMirror.getDoc().setValue(theCode);
+
+            }
+
+            //call the create account endpoint
+            xhrcall(1, `backpage-projects/${projectid}`, "", "json", "", xhrDone2, token)
+
+        }
+        //build the json
+        let bodyobj = {
+            user: {
+                id: 2
+            }
+
+        }
+        //string it
+        var bodyobjectjson = JSON.stringify(bodyobj);
+        //call the create account endpoint
+        xhrcall(1, "backpages/?user=1", bodyobj, "json", "", xhrDone, token)
+    }
+    else
+    {
+      let error = document.getElementById('accountsAlert');
+      error.innerHTML = "project not found"
+      error.classList.remove('d-none'); 
+    }
+
+
+
+
+
+    /*
+    if (urlParam != "") {
+        projectid = urlParam;
+        let xhrDone = (res) => {
+            //parse the response
+            res = JSON.parse(res)
             let theCode = res.data.attributes.template;
             if (theCode == null) {
                 theCode = html5layout
@@ -64,8 +127,8 @@ whenDocumentReady(isReady = () => {
 
         //call the create account endpoint
         xhrcall(1, `backpage-projects/${projectid}`, "", "json", "", xhrDone, token)
-
     }
+    */
 })
 
 document.getElementById('btn-template').addEventListener('click', function() {
@@ -89,14 +152,3 @@ document.getElementById('btn-template').addEventListener('click', function() {
     xhrcall(4, `backpage-projects/${projectid}/`, bodyobjectjson, "json", "", xhrDone, token)
 
 });
-
-
-
-
-/* todo
-
-
-*/
-//document.getElementById('inp-projectemplate').textContent = html5layout
-
-// $('#summernote').summernote("code", html5layout);
