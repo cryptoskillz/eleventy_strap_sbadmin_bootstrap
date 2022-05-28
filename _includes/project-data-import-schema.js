9d0c8af0a31a674abf92f467fe945a46aef0ab9d
chris <cryptoskillz@chriss-MacBook-Air.local>
2022-05-28 15:34:43 +0700

    update original fields

--- _includes/project-data-import-schema.js
+++ _includes/project-data-import-schema.js
@@ -8,6 +8,7 @@ if schema is blank get it from the keys
 
 */
 let fields;
+let originalfields;
 let projectid;
 let whenDocumentReady = (f) => {
     /in/.test(document.readyState) ? setTimeout('whenDocumentReady(' + f + ')', 9) : f()
@@ -23,8 +24,9 @@ whenDocumentReady(isReady = () => {
             //parse the response
             res = JSON.parse(res)
             fields = res.data.attributes.schema.fields.split(',')
+            originalfields = res.data.attributes.schema.originalfields;
             let inpHtml = "";
-            document.getElementById('originalschema').innerHTML = `Leave fields blnk to remove<br>Fields in the imported data ${res.data.attributes.schema.fields}`
+            document.getElementById('originalschema').innerHTML = `Leave fields blnk to remove<br>Fields in the imported data ${res.data.attributes.schema.originalfields}`
             for (var i = 0; i < fields.length; ++i) {
                 //console.log(fields[i])
                 //let theData = res.data.attributes.data[keys[i]]
@@ -72,24 +74,27 @@ document.getElementById('btn-edit').addEventListener('click', function() {
     }
     let inpValue = "";
     let json = {}
+
     //console.log(keys)
     for (var i = 0; i < fields.length; ++i) {
         let tmp = document.getElementById("inp-" + fields[i]).value;
         //console.log(inpValue);
         //data[keys[i]] = inpValue;
-        if (inpValue == "")
-            inpValue = inpValue + tmp
-        else
-            inpValue = inpValue + ',' + tmp
+        if (tmp != "") {
+            if (inpValue == "")
+                inpValue = inpValue + tmp
+            else
+                inpValue = inpValue + ',' + tmp
+        }
     }
     //console.log(inpValue)
-    let tmp2 = { fields: inpValue }
+    let tmp2 = { fields: inpValue,originalfields:originalfields }
     
     console.log(tmp2)
 
     let bodyobj = {
         data: {
-            schema: tmp2
+            schema: tmp2,
         }
     }
     var bodyobjectjson = JSON.stringify(bodyobj);
