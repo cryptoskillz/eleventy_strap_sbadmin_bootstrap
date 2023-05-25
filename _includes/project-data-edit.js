18fda2106e0b5eed7939b0a9fb322d29d7c836a9
chris <cryptoskillz@chriss-MacBook-Air.local>
2023-05-25 14:29:50 +0700

    refactored the project data edit code

--- _includes/project-data-edit.js
+++ _includes/project-data-edit.js
@@ -1,5 +1,6 @@
 let project = JSON.parse(window.localStorage.currentDataItem);
-
+let projecDatatId = JSON.parse(window.localStorage.currentDataItemId)
+let results;
 let whenDocumentReady = (f) => {
     /in/.test(document.readyState) ? setTimeout('whenDocumentReady(' + f + ')', 9) : f()
 }
@@ -11,23 +12,24 @@ whenDocumentReady(isReady = () => {
         //set at input variable
         let inpHtml = "";
         //prcess the results
-        let results = JSON.parse(res);
+        results = JSON.parse(res);
         //check we have some 
         if ((results.length != 0) && (results != "") && (results != null)) {
             //loop through the results
             for (var i = 0; i < results.data.length; ++i) {
                 //get the row
                 let theRow = results.data[i];
+                console.log(theRow);
                 //loop through the fields
-                for (var i2 = 0; i2 < theRow.length; ++i2) {
-                    if (theRow[i2].isUsed == 1) {
-                        //build the input elements
-                        inpHtml = inpHtml + `<div class="form-group" >
-                                            <label>${theRow[i2].fieldName}</label>
-                                            <input type="text" class="form-control form-control-user" id="inp-${theRow[i2].fieldName}" aria-describedby="emailHelp" placeholder="Enter ${theRow[i2].fieldName}" value="${theRow[i2].fieldValue}">
+
+                if (theRow.isUsed == 1) {
+                    //build the input elements
+                    inpHtml = inpHtml + `<div class="form-group" >
+                                            <label>${theRow.fieldName}</label>
+                                            <input type="text" class="form-control form-control-user" id="inp-${theRow.fieldName}" aria-describedby="emailHelp" placeholder="Enter ${theRow.fieldName}" value="${theRow.fieldValue}">
                                             </div>`
-                    }
                 }
+
             }
             //set the header
             document.getElementById('project-header').innerHTML = `Edit Record`;
@@ -42,46 +44,44 @@ whenDocumentReady(isReady = () => {
 
     }
     //make the call.
-    xhrcall(1, `${apiUrl}projectdata/?projectid=${window.localStorage.currentDataItemId}`, "", "json", "", xhrDone, token)
+    xhrcall(1, `${apiUrl}projectdata/?projectId=&projectDataId=${window.localStorage.currentDataItemId}`, "", "json", "", xhrDone, token)
 })
 
 
 document.getElementById('btn-edit').addEventListener('click', function() {
+    //hold the update data
+    let updateData = [];
+    //loop through the results
+    for (var i = 0; i < results.data.length; ++i) {
+        //get the row
+        let theRow = results.data[i];
+        //check it is used
+        if (theRow.isUsed == 1) {
+            //get the input
+            inpValue = document.getElementById("inp-" + theRow.fieldName).value;
+            //build the object
+            const data = {};
+            data[theRow.fieldName] = inpValue;
+            data.id = theRow.id
+            //add it to the array
+            updateData.push(data);
+        }
+    }
 
-    console.log(project)
+    //build the payload
+    let bodyobj = {
+        data: updateData,
+        projectid: project.id,
+    }
+    var bodyobjectjson = JSON.stringify(bodyobj);
 
-/*
+    //process the XHR call
     let xhrDone = (res) => {
         //parse the response
         res = JSON.parse(res);
         showAlert(res.message, 1);
-        updateProjectAllData(res.data);
     }
-    let data = {};
-
-    let currentproject = getCurrentProject();
-    let currentprojectdata = getCurrentProjectData();
-
-    let projectfields = currentproject.schema.fields.split(",");
-    let projectoriginalfields = currentproject.schema.originalfields.split(",")
-    for (var i = 0; i < projectfields.length; ++i) {
-        let inpValue = "";
-        if (projectfields[i] == "UNUSED") {
-            data[projectoriginalfields[i]] = inpValue;
-
-        } else {
-            inpValue = document.getElementById("inp-" + projectfields[i]).value;
-            data[projectfields[i]] = inpValue;
-
-        }
-    }
-    let bodyobj = {
-        data: data,
-        projectid: currentproject.id,
-        dataid: currentprojectdata.id
-    }
-    var bodyobjectjson = JSON.stringify(bodyobj);
-    xhrcall(4, `api/projectdata/`, bodyobjectjson, "json", "", xhrDone, token)
-
-    */
+    
+    //call the API
+    xhrcall(4, `${apiUrl}projectdata/`, bodyobjectjson, "json", "", xhrDone, token)
 })
--- functions/api/projectdata.js
+++ functions/api/projectdata.js
@@ -32,14 +32,27 @@ let buildDataArray = (theData, theId = "") => {
 
 
 let decodeJwt = async (req, secret) => {
-    let bearer = req.get('authorization')
-    bearer = bearer.replace("Bearer ", "");
-    let details = await jwt.decode(bearer, secret)
-    return (details)
+    //get the bearer token
+    let bearer = req.get('authorization');
+    //check they sent a bearer token
+    if (bearer == null) {
+        //send blank
+        return ("")
+    } else {
+        //check if its a bearer token
+        try {
+            let details = await jwt.decode(bearer, secret)
+            return (details)
+        } catch (error) {
+            console.log(error)
+            return("")
+        }
+
+    }
 }
 
 export async function onRequestPut(context) {
-    const jwt = require('@tsndr/cloudflare-worker-jwt')
+    //build the paramaters
     const {
         request, // same as existing Worker API
         env, // same as existing Worker API
@@ -48,29 +61,56 @@ export async function onRequestPut(context) {
         next, // used for middleware or to fetch assets
         data, // arbitrary space for passing data between middlewares
     } = context;
-    let payLoad;
-    let projectName = "";
-    const contentType = request.headers.get('content-type')
-    if (contentType != null) {
-        payLoad = await request.json();
+    //decode the token
+    let token = await decodeJwt(request.headers, env.SECRET);
+    //check its valid
+    if (token == "") {
+        return new Response(JSON.stringify({ error: "not authorised to update records" }), { status: 400 });
+    } else {
+        //get the content type
+        const contentType = request.headers.get('content-type')
+        //set a data array
+        let theData;
+        //set a query values var
+        let theQueryValues = "updatedAt = CURRENT_TIMESTAMP";
+        //set a query where
+        let theQueryWhere = "";
+        //check we have a content type
+        if (contentType != null) {
+            //get the data
+            theData = await request.json();
+            //loop through the data
+            for (var i = 0; i < theData.data.length; ++i) {
+                //build the update query
+                let theQuery = `UPDATE projectData SET `
+                //loop through the field values
+                for (const key in theData.data[i]) {
+                    //set the data
+                    const tdata = theData.data[i];
+                    //check it is not an id or table 
+                    if ((key != "table") && (key != "id")) {
+                        //build the fields
+                        theQueryValues = theQueryValues + `,fieldValue = '${tdata[key]}' `
+                    }
+                    //check for ad id and add a put.
+                    if (key == "id")
+                        theQueryWhere = ` where id = '${tdata[key]}'`
 
+                }
+                //build the query
+                theQuery = theQuery + theQueryValues + ` ${theQueryWhere}`;
+                //return it
+                const info = await context.env.DB.prepare(theQuery).run();
+            }
+            //return
+            return new Response(JSON.stringify({ message: `The record has been updated` }), { status: 200 });
+        }
     }
-    //decode jwt
-    let details = await decodeJwt(request.headers, env.SECRET)
-    //check for projects
-    const KV = context.env.backpage;
-    let kvname = "projects-data" + details.username + "*" + payLoad.projectid + "*" + payLoad.dataid;
 
-    let projectData = { data: "", id: "" }
-    projectData.id = payLoad.dataid
-    projectData.data = payLoad.data
-    projectData = JSON.stringify(projectData)
-    //await KV.delete(kvname);
-    await KV.put(kvname, projectData);
-    return new Response(JSON.stringify({ message: "Item updated", data: projectData }), { status: 200 });
 
 
 }
+
 export async function onRequestPost(context) {
     const {
         request, // same as existing Worker API
@@ -129,6 +169,9 @@ export async function onRequestDelete(context) {
 }
 
 
+
+
+
 export async function onRequestGet(context) {
     const {
         request, // same as existing Worker API
@@ -139,38 +182,52 @@ export async function onRequestGet(context) {
         data, // arbitrary space for passing data between middlewares
     } = context;
     try {
+        //make a data object
+        let finData = {};
         //set the results object
         let results = []
         //get the search params
         const { searchParams } = new URL(request.url);
         //get the project id
         let projectId = searchParams.get('projectid');
-        //get the schema
-        const query = context.env.DB.prepare(`SELECT id,isUsed,fieldName from projectSchema where projectId = '${projectId}'`);
-        const queryResults = await query.all();
-        //get the projects and group by id
-        const query2 = context.env.DB.prepare(`SELECT projectData.projectDataId from projectData where projectData.projectId = '${projectId}' group by projectDataId `);
-        //get the results
-        const queryResults2 = await query2.all();
-        //loop through the projectdata results
-        for (var i = 0; i < queryResults2.results.length; ++i) {
-            //get the id
-            let projectDataId = queryResults2.results[i].projectDataId;
-            //get the data
-            const query3 = context.env.DB.prepare(`SELECT projectData.id,projectData.projectDataId,projectData.schemaId,projectSchema.isUsed,projectSchema.fieldName,projectData.fieldValue from projectData LEFT JOIN projectSchema ON projectData.schemaId = projectSchema.id where projectData.projectDataId = '${projectDataId}' `);
+        console.log(projectId)
+        //get the project dataId 
+        const projectDataId = searchParams.get('projectDataId');
+        if (projectId != null) {
+            //get the schema
+            const query = context.env.DB.prepare(`SELECT id,isUsed,fieldName from projectSchema where projectId = '${projectId}'`);
+            const queryResults = await query.all();
+            //get the projects and group by id
+            const query2 = context.env.DB.prepare(`SELECT projectData.projectDataId from projectData where projectData.projectId = '${projectId}' group by projectDataId `);
             //get the results
-            const queryResults3 = await query3.all();
+            const queryResults2 = await query2.all();
+            //loop through the projectdata results
+            for (var i = 0; i < queryResults2.results.length; ++i) {
+                //get the id
+                let projectDataId = queryResults2.results[i].projectDataId;
+                //get the data
+                const query3 = context.env.DB.prepare(`SELECT projectData.id,projectData.projectDataId,projectData.schemaId,projectSchema.isUsed,projectSchema.fieldName,projectData.fieldValue from projectData LEFT JOIN projectSchema ON projectData.schemaId = projectSchema.id where projectData.projectDataId = '${projectDataId}' `);
+                //get the results
+                const queryResults3 = await query3.all();
+                //put them into our array
+                results.push(queryResults3.results);
+                //debugs
+                //console.log(queryResults3.results)
+            }
+
+            //store the schema
+            finData.schema = queryResults.results;
+            //store the results. 
+            finData.data = results;
+        } else {
+            //return the one project
+            const queryData = context.env.DB.prepare(`SELECT projectData.id,projectData.projectDataId,projectData.schemaId,projectSchema.isUsed,projectSchema.fieldName,projectData.fieldValue from projectData LEFT JOIN projectSchema ON projectData.schemaId = projectSchema.id where projectData.projectDataId = '${projectDataId}' `);
+            //get the results
+            const queryDataResults = await queryData.all();
             //put them into our array
-            results.push(queryResults3.results);
-            //debugs
-            //console.log(queryResults3.results)
+            //results.push(queryDataResults.results);
+            finData.data = queryDataResults.results;
         }
-        //make a data object
-        let finData = {};
-        //store the schema
-        finData.schema = queryResults.results;
-        //store the results. 
-        finData.data = results;
         //debug
         //console.log(queryResults)
         //console.log(queryResults2)
